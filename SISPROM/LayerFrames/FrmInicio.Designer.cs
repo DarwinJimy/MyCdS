da9b32702a295d0db680a09db9d55ad446c44177
DarwinJimy <djfloresq@gmail.com>
2015-05-01 15:38:59 -0500

    new changes

--- SISPROM/LayerFrames/FrmInicio.Designer.cs
+++ SISPROM/LayerFrames/FrmInicio.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmInicio));
             this.panel1 = new System.Windows.Forms.Panel();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
@@ -116,6 +117,9 @@
             this.ClientSize = new System.Drawing.Size(685, 303);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "FrmInicio";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Login SISPROM";
